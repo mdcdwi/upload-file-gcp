d80090a9cd6ab60e93ee352b3591a3293a5df6d7
lucyfyy <bambang.nalann@gmail.com>
2023-01-28 02:03:10 +0700

    fix try

--- main.py
+++ main.py
@@ -8,15 +8,16 @@ def main():
     try:
         GOOGLE_STORAGE_CREDENTIAL = os.environ.get("GOOGLE_STORAGE_CREDENTIAL")
         GOOGLE_STORAGE_NAME = os.environ.get("GOOGLE_STORAGE_NAME")
+        try:
+            request_data = request.get_json()
+            file = request_data['file']
+            filename = request_data['filename']
+            upload.upload(file, filename, GOOGLE_STORAGE_NAME, GOOGLE_STORAGE_CREDENTIAL)
+            return json.dumps({'Status': 'Success', 'data': 'https://storage.googleapis.com/'+ GOOGLE_STORAGE_NAME +'/'+filename}, status=200)
+        except (Exception) as e:
+            return json.dumps({'status': 'Failed to upload', 'message': str(e)}, status=400)
     except (Exception) as e:
         return json.dumps({'Status': 'Failed to load env', 'message': str(e)}, status=400)
-    try:
-        request_data = request.get_json()
-        file = request_data['file']
-        filename = request_data['filename']
-        upload.upload(file, filename, GOOGLE_STORAGE_NAME, GOOGLE_STORAGE_CREDENTIAL)
-    except (Exception) as e:
-        return json.dumps({'status': 'Failed to upload', 'message': str(e)}, status=400)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="8080", debug=True)
